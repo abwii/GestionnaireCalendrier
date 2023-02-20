a97955a9cab85113c6e19632181c2b88dacef705
Nathan L <101799411+ehcraball@users.noreply.github.com>
2023-02-20 10:23:09 +0100

    Add files via upload

--- src/pythonCalendar/py/create_event.py
+++ src/pythonCalendar/py/create_event.py
@@ -1,39 +1,15 @@
 import os.path
 import json
-import pickle
 from os import path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from datetime import datetime, timedelta
+from cal_setup import get_calendar_service
 
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-
-CREDENTIALS_FILE = 'src\pythonCalendar\py\json\code_api.json'
-
-def get_calendar_service():
-   creds = None
-   if os.path.exists('token.pickle'):
-       with open('token.pickle', 'rb') as token:
-           creds = pickle.load(token)
-   if not creds or not creds.valid:
-       if creds and creds.expired and creds.refresh_token:
-           creds.refresh(Request())
-       else:
-           flow = InstalledAppFlow.from_client_secrets_file(
-               CREDENTIALS_FILE, SCOPES)
-           creds = flow.run_local_server(port=0)
-       with open('token.pickle', 'wb') as token:
-           pickle.dump(creds, token)
-
-   service = build('calendar', 'v3', credentials=creds)
-   return service
 
 def main():
 
    idCalendar = input("Entrez l'id de votre agenda : ('p' si agenda principal)")
    if (idCalendar == 'p') :
       idCalendar = 'primary'
-   
 
    service = get_calendar_service()
 
