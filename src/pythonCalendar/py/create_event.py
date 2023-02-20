28de454abc3ea6f87876ee0c55208dd8e3a5c11d
Wassim <66485319+abwii@users.noreply.github.com>
2023-02-20 04:25:39 +0100

    100% google api :)

--- src/pythonCalendar/py/create_event.py
+++ src/pythonCalendar/py/create_event.py
@@ -1,20 +1,128 @@
+import os.path
+import json
+from os import path
 from datetime import datetime, timedelta
 from cal_setup import get_calendar_service
 
 
 def main():
+
    service = get_calendar_service()
 
-   #requête api
-   event_result = service.events().insert(calendarId='primary',
-       body={
-           "summary": 'Fête de noel',
-           "description": 'Fêtes des neiges',
-           "start": {"dateTime": "2023-02-20T10:00:00+01:00", "timeZone": 'UTC+1'},
-           "end": {"dateTime": "2023-02-20T10:00:00+01:00", "timeZone": 'UTC+1'},
-       }
-   ).execute()
-   #fin requête
+   idCalendar = 'primary'
+
+   if (path.exists("resultatA.json") == True) :
+      resultatA = open("resultatA.json", "r")
+      print("existe A")
+      with open('resultatA.json', 'r') as f:
+         dataA = json.load(f)
+         print(len(dataA))
+   else :
+      dataA = None
+
+   if (path.exists("resultatB.json") == True) :
+      resultatA = open("resultatB.json", "r")
+      print("existe B ")
+      with open('resultatB.json', 'r') as f:
+         dataB = json.load(f)
+         print(len(dataB))
+   else :
+      dataB = None
+
+   if (path.exists("resultatC.json") == True) :
+      resultatA = open("resultatC.json", "r")
+      print("existe C")
+      with open('resultatC.json', 'r') as f:
+         dataC = json.load(f)
+         print(len(dataC))
+   else :
+      dataC = None
+
+   if (path.exists("resultatFeries.json") == True) :
+      resultatA = open("resultatA.json", "r")
+      print("existe Ferie")
+      with open('resultatFeries.json', 'r') as f:
+         dataFeries = json.load(f)
+         print(len(dataFeries))
+   else :
+      dataFeries = None
+
+   if (path.exists("resultatA.json") == True) :
+
+      for i in range (0, len(dataA)) :
+
+         a = dataA[i]['summary']
+         b = dataA[i]['description']
+         c = dataA[i]['start']
+         d = dataA[i]['end']
+
+         event_result = service.events().insert(
+            calendarId=idCalendar,
+            body={
+               "summary": a,
+               "description": b,
+               "start": {"dateTime": c, "timeZone": 'UTC+1'},
+               "end": {"dateTime": d, "timeZone": 'UTC+1'},
+            }
+         ).execute()
+
+   if (path.exists("resultatB.json") == True) :
+
+      for i in range (0, len(dataB)) :
+
+         a = dataB[i]['summary']
+         b = dataB[i]['description']
+         c = dataB[i]['start']
+         d = dataB[i]['end']
+
+         event_result = service.events().insert(
+            calendarId=idCalendar,
+            body={
+               "summary": a,
+               "description": b,
+               "start": {"dateTime": c, "timeZone": 'UTC+1'},
+               "end": {"dateTime": d, "timeZone": 'UTC+1'},
+            }
+         ).execute()
+
+   if (path.exists("resultatC.json") == True) :
+
+      for i in range (0, len(dataC)) :
+
+         a = dataC[i]['summary']
+         b = dataC[i]['description']
+         c = dataC[i]['start']
+         d = dataC[i]['end']
+
+         event_result = service.events().insert(
+            calendarId=idCalendar,
+            body={
+               "summary": a,
+               "description": b,
+               "start": {"dateTime": c, "timeZone": 'UTC+1'},
+               "end": {"dateTime": d, "timeZone": 'UTC+1'},
+            }
+         ).execute()
+
+   if (path.exists("resultatFeries.json") == True) :
+
+      for i in range (0, len(dataFeries)) :
+
+         a = dataFeries[i]['summary']
+         b = dataFeries[i]['description']
+         c = dataFeries[i]['start']
+         d = dataFeries[i]['end']
+
+         event_result = service.events().insert(
+            calendarId=idCalendar,
+            body={
+               "summary": a,
+               "description": b,
+               "start": {"dateTime": c, "timeZone": 'UTC+1'},
+               "end": {"dateTime": d, "timeZone": 'UTC+1'},
+            }
+         ).execute()
+   
 
    print("Succès")
 
